61af19a94b385a854f4b0567ed0d78dcd13a0fc9
Cahit Arslan <cahitarslan.business@gmail.com>
2024-02-07 04:33:25 +0300

    Add project files.

---
+++ Task1-MaskeTakip/Workaround/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Workaround
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Degiskenler();
+
+            Vatandas vatandas1 = new();
+        }
+
+        private static void Degiskenler()
+        {
+            string mesaj = "Merhaba";
+            double tutar = 100000;
+            int sayi = 100;
+            bool girisYapmisMi = false;
+
+            string ad = "Cahit";
+            string soyad = "Arslan";
+            int dogumYili = 1993;
+            long tcNo = 12345678910;
+
+            Console.WriteLine(tutar * 1.18);
+
+            Console.WriteLine(tutar * 1.18);
+        }
+    }
+
+    public class Vatandas
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int DogumYili { get; set; }
+        public long TcNo { get; set; }
+    }
+}
