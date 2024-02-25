be86edccc06e782cb32442cc4771a998a9a6e7ce
Cahit Arslan <cahitarslan.business@gmail.com>
2024-02-25 15:46:16 +0300

    Finish Task2.4

---
+++ Task2/Task2.4-nArchitecture/src/demoProjects/rentACar/Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,28 @@
+﻿using Application.Services.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Contexts;
+using Persistence.Repositories;
+
+namespace Persistence;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services,
+                                                            IConfiguration configuration)
+    {
+        services.AddDbContext<BaseDbContext>(options =>
+                                                 options.UseSqlServer(
+                                                     configuration.GetConnectionString("RentACarConnectionString")));
+
+        services.AddScoped<IBrandRepository, BrandRepository>();
+        services.AddScoped<IModelRepository, ModelRepository>();
+        services.AddScoped<IOperationClaimRepository, OperationClaimRepository>();
+        services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+        services.AddScoped<IUserOperationClaimRepository, UserOperationClaimRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+
+        return services;
+    }
+}
